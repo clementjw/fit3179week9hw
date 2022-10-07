0bc8e5c99f265ef7fd818b029ba6bdc5d2725c83
Jia Wen <64416000+clementjw@users.noreply.github.com>
2022-10-07 21:47:04 +0800

    Update mapembed.js

--- mapembed.js
+++ mapembed.js
@@ -1,2 +1,4 @@
 var vg_1 = "map.json";
+var vg_2 = "lineg.json";
 vegaEmbed("#chloroplethmap", vg_1).then(function(result) {}).catch(console.error);
+vegaEmbed("#linegraph", vg_2).then(function(result) {}).catch(console.error);
